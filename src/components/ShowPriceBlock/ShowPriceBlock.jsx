20c4a1cd89b51e41b9f8c8ae17b937422d7e72c0
SviatoslaKondratiuk <ipz241_ksr@student.ztu.edu.ua>
2025-11-30 21:07:35 +0200

    added showblockprice

--- src/components/ShowPriceBlock/ShowPriceBlock.jsx
+++ src/components/ShowPriceBlock/ShowPriceBlock.jsx
@@ -7,19 +7,32 @@ export default function ShowPriceBlock({ object }) {
 
 
 
+    const entries = Object.entries(object).filter(([key]) => key !== "name");
 
     return (
-        <>
-            <section className="show_prices">
-                {object.name
+
+        <section className="show_prices">
+
+            <div className="grid_price">
+
+                {entries.map(([serviceName, price]) => (
+
+                    < div className="block" >
+                        <div className="showname">
+                            {serviceName}
+                        </div>
+
+                        <div className="showprice">
+                            {price}
+
+                        </div>
+                    </div >
+                ))}
+
+            </div >
 
 
-                }
+        </section >
 
-            </section>
-
-
-
-        </>
     )
 }
