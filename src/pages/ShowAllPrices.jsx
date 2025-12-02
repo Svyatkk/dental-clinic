d89f75d81c496997504cf8ad950e2a0593c046ab
SviatoslaKondratiuk <ipz241_ksr@student.ztu.edu.ua>
2025-12-02 13:41:59 +0200

    update

--- src/pages/ShowAllPrices.jsx
+++ src/pages/ShowAllPrices.jsx
@@ -5,39 +5,39 @@ import ShowPriceBlock from "../components/ShowPriceBlock/ShowPriceBlock"
 import { infoServicePages } from "../components/Data/DataServicePrices"
 import ShowTransition from "../components/ShowTransition/ShowTransition"
 
-
-
-
-
-
 export default function ShowAllPrices() {
 
-    const [open, setOpen] = useState(null)
+    const [open, setOpen] = useState([])
+
+
+
 
     function onclick(i) {
+        if (open.includes(i)) {
+            setOpen(open.filter(item => item !== i))
+        }
+        else {
+            setOpen([...open, i])
 
-        setOpen(open === i ? null : i)
+        }
     }
 
     function generateForAccordeon(objects) {
         return objects.map((element, index) => (
             <div
-                className={`accordeon ${open === index ? "show" : ""}`}
-                onClick={() => onclick(index)}
+                className={`accordeon ${open.includes(index) ? "show" : ""}`}
+                onClick={() =>
+                    onclick(index)}
                 key={index}>
                 <h3>{element.name}</h3>
 
                 <div
-
-
                     className="show_info">
                     <ShowPriceBlock object={element} />
                 </div>
 
-
                 <span>
                     &#62;
-
                 </span>
 
             </div>
@@ -45,16 +45,10 @@ export default function ShowAllPrices() {
     }
     const name_of_page = `Ціни`
 
-
-
     return (
         <>
-
-
             <Navbar />
             <ShowTransition page={name_of_page}></ShowTransition>
-
-
             <div className="container_for_accordeon">
                 {generateForAccordeon(infoServicePages)}
 
